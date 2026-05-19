357cfa4df08d333ac0fdacff89d79523924c5fef
chaixshot <chai5142xshot@gmail.com>
2026-05-19 14:31:35 +0700

    Changed: patch LaserPointer for both hand

--- Patches/LaserPointer.cs
+++ Patches/LaserPointer.cs
@@ -1,4 +1,6 @@
 ﻿using HarmonyLib;
+using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using XSOverlay;
 
@@ -7,57 +9,145 @@ namespace xsoverlay_tweak.Patches
     [HarmonyPatch(typeof(Raycaster))]
     internal class LaserPointer
     {
-        private static Unity_Overlay Laser;
-        private static float distance;
+        private class LaserData
+        {
+            public Unity_Overlay Laser;
+            public float Distance;
+            public float Distance_Last;
+        }
+        private static readonly ConditionalWeakTable<Raycaster, LaserData> LaserDictionary = new();
 
+        // Create laser overlay
         [HarmonyPatch("Start")]
         [HarmonyPostfix]
-        public static void Start(Raycaster __instance, ref GameObject ___VisualCursorElement, ref Unity_Overlay ___VisualCursorElementOverlay)
+        public static void Start(Raycaster __instance)
         {
-            if (Laser != null) return;
+            if (!IsHand(__instance)) return;
+            if (IsEnable())
+                CreateLaser(__instance);
 
-            Laser = GameObject.Instantiate(___VisualCursorElementOverlay);
-            Object.Destroy(Laser.GetComponent<UI_RelativeTransformManipulator>());
+            // Listen for hovering state changes to update laser length immediately when hovering something new
+            XSOEventSystem.OnSwitchHoveringOverlay += (hovering, overlay) =>
+            {
+                if (IsEnable())
+                    Plugin.Instance.StartCoroutine(UpdateLaserLengthDelay(hovering));
+            };
 
-            Laser.name = $"{___VisualCursorElementOverlay.name}.laser";
-            Laser.overlayName = $"{___VisualCursorElementOverlay.overlayName}.laser";
-            Laser.overlayKey = $"{___VisualCursorElementOverlay.overlayKey.ToLower()}.laser";
-            Laser.overlayTexture = new Texture2D(1, 1, TextureFormat.RGB24, false);
-            Laser.AutoUpdateOverlayTexture = false;
+            // Listen for setting changes to create/destroy lasers when toggling the setting
+            XConfig.LaserPointer.SettingChanged += (sender, args) =>
+            {
+                if (IsEnable())
+                    CreateLaser(__instance);
+                else
+                {
+                    if (LaserDictionary.TryGetValue(__instance, out LaserData Data))
+                        Object.Destroy(Data.Laser);
+                    LaserDictionary.Remove(__instance);
+                }
 
-            Laser.gameObject.SetActive(true);
+            };
         }
 
-        [HarmonyPatch("SetVisualCursorTransform")]
+        // Check should render lasers
+        [HarmonyPatch("DetermineIfActiveRaycaster")]
         [HarmonyPostfix]
-        public static void SetVisualCursorTransform(Raycaster __instance, ref Vector3 ___CurrentRayPosition, ref Vector3 ___RayHitPoint, ref Vector3 ___CurrentRayDirection)
+        public static void DetermineIfActiveRaycaster(Raycaster __instance)
         {
-            if (Laser == null) return;
-            if (DesktopCursorManager.Instance.GetCurrentInputDevice() != __instance) return;
+            if (!IsEnable()) return;
+            if (!IsHand(__instance)) return;
 
-            distance = Vector3.Distance(___CurrentRayPosition, ___RayHitPoint);
+            if (Overlay_Manager.Instance.editMode || __instance.HoveringOverlay != null)
+            {
+                __instance.IsActiveRaycaster = true;
 
-            var headPos = Overlay_Manager.Instance.head.position;
-
-            Laser.transform.position = ___CurrentRayPosition + (___CurrentRayDirection * (distance / 2));
-            Laser.transform.up = ___CurrentRayDirection;
-            //Laser.transform.localScale = new Vector3(1, distance, 1);
-
-            Laser.colorTint = Color.green;
-
-
-            int Tickness = 300;
-
-            Laser.overlayTexture = new Texture2D(1, (int)distance * Tickness, TextureFormat.RGB24, false);
-            Laser.overlay.overlayTexture = Laser.overlayTexture;
-            Laser.overlay.overlayWidthInMeters = distance / Tickness;
+                if (LaserDictionary.TryGetValue(__instance, out LaserData Data))
+                    Data.Laser.gameObject.SetActive(true);
+            }
+            else
+                if (LaserDictionary.TryGetValue(__instance, out LaserData Data))
+                    Data.Laser.gameObject.SetActive(false);
         }
 
-        /*[HarmonyPatch("ReleasePointer")]
+        // Change lasers position, rotation and length
+        [HarmonyPatch("UpdateRaycaster")]
         [HarmonyPostfix]
-        public static void ReleasePointer(Raycaster __instance)
+        public static void UpdateRaycaster(Raycaster __instance, ref Vector3 ___CurrentRayPosition, ref Vector3 ___RayHitPoint, ref Vector3 ___CurrentRayDirection)
         {
-            
-        }*/
+            if (!IsEnable()) return;
+            if (!IsHand(__instance)) return;
+
+            if (LaserDictionary.TryGetValue(__instance, out LaserData Data))
+            {
+                Vector3 RayHitPoint = ___RayHitPoint - (___CurrentRayDirection * 0.04f);
+
+                Data.Distance = Vector3.Distance(___CurrentRayPosition, RayHitPoint);
+
+                Data.Laser.transform.position = ___CurrentRayPosition + (___CurrentRayDirection * (Data.Distance / 2));
+                Data.Laser.transform.up = ___CurrentRayDirection;
+
+                if (Mathf.Abs(Data.Distance_Last - Data.Distance) > 0.05f)
+                    UpdateLaserLength(__instance);
+            }
+        }
+
+        // ActivePointerColor
+        [HarmonyPatch("UpdateHoveringOverlay")]
+        [HarmonyPostfix]
+        public static void UpdateHoveringOverlay(Raycaster __instance, ref Unity_Overlay ___VisualCursorElementOverlay)
+        {
+            if (!IsEnable()) return;
+            if (!IsHand(__instance)) return;
+
+            if (XConfig.ActivePointerColor.Value)
+                if (LaserDictionary.TryGetValue(__instance, out LaserData Data))
+                    Data.Laser.colorTint = ___VisualCursorElementOverlay.colorTint;
+        }
+
+        private static void CreateLaser(Raycaster instance)
+        {
+            Unity_Overlay VisualCursorElementOverlay = (Unity_Overlay)AccessTools.Field(typeof(Raycaster), "VisualCursorElementOverlay").GetValue(instance);
+            Unity_Overlay laser = Object.Instantiate(VisualCursorElementOverlay);
+
+            Object.Destroy(laser.GetComponent<UI_RelativeTransformManipulator>());
+
+            laser.AutoUpdateOverlayTexture = false;
+            laser.name = VisualCursorElementOverlay.name.Replace("VisualCursor", "Laser");
+            laser.overlayName = VisualCursorElementOverlay.overlayName.Replace("VisualCursor", "Laser");
+            laser.overlayKey = VisualCursorElementOverlay.overlayKey.Replace("VisualCursor", "Laser");
+            laser.overlayTexture = new Texture2D(1, 1, TextureFormat.RGB24, false);
+            laser.overlay.overlayTexture = laser.overlayTexture;
+
+            LaserDictionary.Add(instance, new LaserData { Laser = laser, Distance = 1f, Distance_Last = 1f });
+        }
+
+        // Wait one frame for UpdateRaycaster to update Distance
+        private static IEnumerator UpdateLaserLengthDelay(Raycaster hovering)
+        {
+            yield return null;
+            UpdateLaserLength(hovering);
+        }
+
+        private static void UpdateLaserLength(Raycaster hovering)
+        {
+            if (LaserDictionary.TryGetValue(hovering, out LaserData Data))
+            {
+                Object.Destroy(Data.Laser.overlayTexture);
+
+                Data.Laser.overlayTexture = new Texture2D(1, (int)(Data.Distance * 500), TextureFormat.RGB24, false);
+                Data.Laser.overlay.overlayTexture = Data.Laser.overlayTexture;
+                Data.Laser.overlay.overlayWidthInMeters = 0.002f;
+                Data.Distance_Last = Data.Distance;
+            }
+        }
+
+        private static bool IsEnable()
+        {
+            return XConfig.LaserPointer.Value;
+        }
+
+        private static bool IsHand(Raycaster __instance)
+        {
+            return __instance.HapticDeviceName != Raycaster.HapticDevice.None;
+        }
     }
 }
