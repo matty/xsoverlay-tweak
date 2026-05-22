cb4b23bd04f26e4d5cb0a46677107a16f9b4b540
chaixshot <chai5142xshot@gmail.com>
2026-05-22 19:55:39 +0700

    Fixed: LaserPointer face rotation

--- Patches/LaserPointer.cs
+++ Patches/LaserPointer.cs
@@ -101,8 +101,7 @@ namespace xsoverlay_tweak.Patches
                     Data.Laser.transform.position = CurrentRayPosition + (CurrentRayDirection * (Data.Distance / 2));
 
                     Data.Laser.transform.up = CurrentRayDirection;
-                    if (!IsRightHand(__instance))
-                        Data.Laser.transform.Rotate(0, -45, 0, Space.Self);
+                    Data.Laser.transform.Rotate(0, 180 * Overlay_Manager.Instance.head.rotation.y, 0, Space.Self);
 
                     if (Mathf.Abs(Data.Distance_Last - Data.Distance) > 0.01f)
                         UpdateLaserLength(__instance);
