a1c31fddecd95cd497dd7526340a82d1fb6878ec
chaixshot <chai5142xshot@gmail.com>
2026-05-23 12:55:20 +0700

    Changed: LaserPointer face up calculation by its Y and Head Y

--- Patches/LaserPointer.cs
+++ Patches/LaserPointer.cs
@@ -101,7 +101,7 @@ namespace xsoverlay_tweak.Patches
                     Data.Laser.transform.position = CurrentRayPosition + (CurrentRayDirection * (Data.Distance / 2));
 
                     Data.Laser.transform.up = CurrentRayDirection;
-                    Data.Laser.transform.Rotate(0, 180 * __instance.transform.rotation.y, 0, Space.Self);
+                    Data.Laser.transform.Rotate(0, 180 * (__instance.transform.rotation.y - (__instance.transform.rotation.y - Overlay_Manager.Instance.head.rotation.y)), 0, Space.Self);
 
                     if (Mathf.Abs(Data.Distance_Last - Data.Distance) > 0.01f)
                         UpdateLaserLength(__instance);
