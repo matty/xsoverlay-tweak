cba23d75987dbe83c3c02c057ee9bc964a2c3b41
chaixshot <chai5142xshot@gmail.com>
2026-05-23 12:56:34 +0700

    Changed: improved AlwaysHideCursor

--- Patches/AlwaysHideCursor.cs
+++ Patches/AlwaysHideCursor.cs
@@ -1,45 +1,21 @@
 ﻿using HarmonyLib;
-using System.Collections.Generic;
 
 namespace xsoverlay_tweak.Patches
 {
+    [HarmonyPatch(typeof(WindowComponentManager))]
     internal class AlwaysHideCursor
     {
-        // Static list to track all active window managers
-        private static readonly List<WindowComponentManager> instanceRefs = [];
-
         // Cache the private field once for high-speed access
         private static readonly AccessTools.FieldRef<WindowComponentManager, bool> WindowCursorRef = AccessTools.FieldRefAccess<WindowComponentManager, bool>("WindowCanShowDesktopCursor");
 
-        [HarmonyPatch(typeof(WindowComponentManager), "Start")]
+        [HarmonyPatch("OnSwitchHoveringOverlay")]
         [HarmonyPostfix]
-        public static void Start(WindowComponentManager __instance)
-        {
-            if (!instanceRefs.Contains(__instance))
-                instanceRefs.Add(__instance);
-        }
-
-        [HarmonyPatch(typeof(WindowComponentManager), "OnSwitchHoveringOverlay")]
-        [HarmonyPostfix]
-        public static void HideCursorForHoveringOverlay()
+        public static void CheckShowCursorPrefix(WindowComponentManager __instance)
         {
             if (!IsEnable()) return;
 
-            // Loop backwards through all managers
-            for (int i = instanceRefs.Count - 1; i >= 0; i--)
-            {
-                WindowComponentManager manager = instanceRefs[i];
-
-                // If window was destroyed, remove from list and skip
-                if (manager == null)
-                {
-                    instanceRefs.RemoveAt(i);
-                    continue;
-                }
-
-                // Set the private boolean to false for EVERY manager
-                WindowCursorRef(manager) = false;
-            }
+            // Set the private boolean to false every frame before the original method checks it
+            WindowCursorRef(__instance) = false;
         }
 
         private static bool IsEnable()
