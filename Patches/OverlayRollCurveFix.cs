fe978c8a6f37cf3f9bd873c5aa884781ee82d298
chaixshot <chai5142xshot@gmail.com>
2026-05-26 08:49:51 +0700

    Changed: OverlayRollCurveFix on spawn in

--- Patches/OverlayRollCurveFix.cs
+++ Patches/OverlayRollCurveFix.cs
@@ -19,9 +19,13 @@ namespace xsoverlay_tweak.Patches
             RollState state = LastX.GetOrCreateValue(overlay);
             Quaternion rotation = overlay.transform.rotation;
 
-            if (rotation.eulerAngles.x > 335f) // Bad angle
-                if (rotation.eulerAngles.x > state.LastRotation) // Moving down
+            if (rotation.eulerAngles.x > 335f) // Overlay rolling change at this angle along with curve
+                if (rotation.eulerAngles.x > state.LastRotation || !overlay.IsHeld) // Moving down or spawn in
+                {
+                    overlay.overlayCachedCurveRadius = 0f;
+
                     return false;
+                }
 
             state.LastRotation = rotation.eulerAngles.x;
 
