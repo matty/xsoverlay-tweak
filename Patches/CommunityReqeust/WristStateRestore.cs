0179647339004eab2ddd3f447d928bb07df39895
chaixshot <chai5142xshot@gmail.com>
2026-06-07 12:18:23 +0700

    Added: patch CommunityReqeust.WristStateRestore

---
+++ Patches/CommunityReqeust/WristStateRestore.cs
@@ -0,0 +1,64 @@
+﻿using HarmonyLib;
+using System.Threading.Tasks;
+using Vuplex.WebView;
+using XSOverlay;
+using XSOverlay.WebApp;
+using xsoverlay_tweak.Utils;
+
+namespace xsoverlay_tweak.Patches.CommunityReqeust
+{
+    internal class WristStateSave
+    {
+
+        [HarmonyPatch(typeof(UpdateDateTime), "Awake")]
+        [HarmonyPostfix]
+        public static void ListenForChanging()
+        {
+            XSOEventSystem.OnStartStopPerformanceMonitor += (enable) =>
+            {
+                CutomSettings.Settings.IsPerformanceMonitorOpened = enable;
+                CutomSettings.SaveSettings();
+            };
+
+            CustomAPI.OnClickToggleMediaPlayer += (enable) =>
+            {
+                CutomSettings.Settings.IsMediaPlayerOpened = enable;
+                CutomSettings.SaveSettings();
+            };
+        }
+
+        [HarmonyPatch(typeof(Overlay_Manager), "OnRegisterWebviewOverlay")]
+        [HarmonyPostfix]
+        public static void RestoreWristState(OverlayWebView wv)
+        {
+            if (wv.UserInterfaceSelection == OverlayWebView.UserInterfacePaths.Wrist)
+            {
+                string jsCode = string.Format(@"(function() {{
+                        if ({0}) {{
+                            MiniToolbar.PerformanceStats.click();
+                        }};
+
+                        if ({1} && !GetShowMediaPlayer()) {{
+                            MiniToolbar.MediaPlayer.click();
+                        }};
+                    }})()", CutomSettings.Settings.IsPerformanceMonitorOpened.ToString().ToLower(), CutomSettings.Settings.IsMediaPlayerOpened.ToString().ToLower());
+
+                wv._webView.WebView.LoadProgressChanged += (sender, args) =>
+                {
+                    if (args.Type == ProgressChangeType.Finished)
+                    {
+                        Task.Run(async () =>
+                        {
+                            await Task.Delay(1000);
+
+                            wv._webView.WebView.ExecuteJavaScript(jsCode, (result) =>
+                            {
+                                //Plugin.Logger.LogError($"[{wv.UserInterfaceSelection}] {result}");
+                            });
+                        });
+                    }
+                };
+            }
+        }
+    }
+}
---
+++ Utils/CutomSettings.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using System.IO;
+using UnityEngine;
+using XSOverlay;
+
+namespace xsoverlay_tweak.Utils
+{
+    internal class CutomSettings
+    {
+        public static XSettings Settings = new();
+
+        public bool IsPerformanceMonitorOpened = false;
+        public bool IsMediaPlayerOpened = false;
+
+        public static void SaveSettings()
+        {
+            string filePath = Application.persistentDataPath + "/config/";
+
+            if (!Directory.Exists(filePath))
+                Directory.CreateDirectory(filePath);
+
+            string contents = JsonUtility.ToJson(Settings, prettyPrint: true);
+            File.WriteAllText(System.IO.Path.Combine(filePath, "tweakSettings.json"), contents);
+        }
+
+        [HarmonyPatch(typeof(XSettingsManager), "LoadSettings")]
+        [HarmonyPostfix]
+        public static void LoadWristStateSettings()
+        {
+            string filePath = Application.persistentDataPath + "/config/tweakSettings.json";
+
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+
+                Settings = JsonUtility.FromJson<XSettings>(json);
+            }
+        }
+
+        [System.Serializable]
+        public class XSettings
+        {
+            public bool IsPerformanceMonitorOpened = false;
+            public bool IsMediaPlayerOpened = false;
+        }
+    }
+}
