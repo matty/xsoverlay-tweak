ae58cfe9881d90bb80dea7b3d2fe60724b44726e
chaixshot <chai5142xshot@gmail.com>
2026-06-15 14:15:31 +0700

    pump version 1.1.0

--- MyPluginInfo.cs
+++ MyPluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string PLUGIN_GUID = "xsoverlay.tweak";
         public const string PLUGIN_NAME = "XSOverlay Tweak";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.1.0";
     }
 }
